c3d1ebfa5261fd1e492a76afca8e8578e579c7ca
Diego H <diego.hernangomezherrero@gmail.com>
2022-01-18 09:10:30 +0100

    Actualiza

--- limpia_crea_apuntes.R
+++ limpia_crea_apuntes.R
@@ -1,6 +1,12 @@
 styler::style_dir(filetype = ".Rmd")
 rmarkdown::render("Apuntes_Master_File.Rmd")
 rmarkdown::render("Apuntes_Master_File.Rmd", 
-                  output_format = rmarkdown::pdf_document(latex_engine = "xelatex"))
+                  output_format = bookdown::pdf_document2(latex_engine = "xelatex",
+                                                          toc = TRUE,
+                                                          toc_depth =  2,
+                                                          number_sections = TRUE,
+                                                          global_numbering = TRUE))
 
+
+bookdown::pd
 
