82b00e9fd26543d3f7af6e1059703a705c4a447f
Diego H <diego.hernangomezherrero@gmail.com>
2022-01-27 09:39:48 +0100

    Separeado la teoria y practica

--- crea-libros.R
+++ crea-libros.R
@@ -4,10 +4,23 @@
 styler::style_dir(filetype = ".Rmd")
 styler::style_dir(filetype = ".R")
 
-bookdown::render_book("index.Rmd", clean = TRUE, envir = new.env())
+# Esto crea el libro en html
 bookdown::render_book("index.Rmd",
-  output_format = "bookdown::pdf_book",
-  envir = new.env()
+  clean = TRUE, envir = new.env()
 )
+
+
+# bookdown::render_book("index.Rmd",
+#   output_format = "bookdown::pdf_book",
+#   envir = new.env()
+# )
+
+# Esto crea las aplicaciones
+rmarkdown::render("99-aplicaciones.Rmd",
+  output_format = "html_document",
+  output_file = "Aplicaciones.html"
+)
+
+# rmd_files: ["index.Rmd", "01-rev_geodatos.Rmd", "02-geocomp.Rmd","03-formatos.Rmd", "04-est_esp.Rmd","05-anexo.Rmd"]
 
 # bookdown::render_book("index.Rmd", output_format = "bookdown::epub_book", envir = new.env())
